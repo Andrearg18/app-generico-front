4b588bef4fd88d5b5db77b01e38a065b6cf397aa
Andrea <andrearg18@educastur.es>
2025-01-05 19:53:54 +0100

    Home done

--- generico/src/app/app.module.ts
+++ generico/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+const ANGULAR_MATERIAL = [
+  MatIconModule,
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +16,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...ANGULAR_MATERIAL,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- generico/src/app/pages/home/home.component.ts
+++ generico/src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: false,
-  
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
